7b27038942a0528a7ba049892ed112e425f3bdb5
felipe thaleson <thaleson19@hotmail.com>
2023-12-21 15:29:16 -0300

    yes

--- comentarios/models.py
+++ comentarios/models.py
@@ -7,7 +7,7 @@ from django.utils import timezone
 class Comentario(models.Model):
     nome_comentario = models.CharField(max_length=150, verbose_name='Nome')
     email_comentario = models.EmailField(verbose_name='E-mail')
-    comentario = models.TextField(verbose_name='Comentário')
+    comentario = models.TextField(max_length=100, verbose_name='Comentários')
     post_comentario = models.ForeignKey(Post, on_delete=models.CASCADE)
     usuario_comentario = models.ForeignKey(User, on_delete=models.DO_NOTHING, blank=True, null=True)
     data_comentario = models.DateTimeField(default=timezone.now)
